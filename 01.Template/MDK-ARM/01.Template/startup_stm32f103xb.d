1bb11d045f1bdb6b3f52a1a7fc59464d4df533f4
eBean <yongbo.dengyb@gmail.com>
2024-11-04 14:00:16 -0800

    Template

---
+++ 01.Template/MDK-ARM/01.Template/startup_stm32f103xb.d
@@ -0,0 +1,1 @@
+01.template\startup_stm32f103xb.o: startup_stm32f103xb.s
---
+++ 01.Template/MDK-ARM/01.Template/tasks.d
@@ -0,0 +1,16 @@
+01.template\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/tasks.c
+01.template\tasks.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+01.template\tasks.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+01.template\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+01.template\tasks.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+01.template\tasks.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+01.template\tasks.o: ../Core/Inc/FreeRTOSConfig.h
+01.template\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+01.template\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+01.template\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+01.template\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM3/portmacro.h
+01.template\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+01.template\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+01.template\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
+01.template\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/timers.h
+01.template\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/stack_macros.h
